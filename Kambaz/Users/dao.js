e25d19205d6a24fff6d8ea61842a62d4e5bb43b1
Chris <125088905+wyattchris@users.noreply.github.com>
2025-12-01 10:59:58 -0500

    a6 work

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -1,20 +1,22 @@
+import model from "./model.js";
 import { v4 as uuidv4 } from "uuid";
-export default function UsersDao(db) {
-    let { users } = db;
+export default function UsersDao() {
     const createUser = (user) => {
         const newUser = { ...user, _id: uuidv4() };
-        users = [...users, newUser];
-        return newUser;
+        return model.create(newUser);
+    }
+    const findAllUsers = () => model.find();
+    const findUserById = (userId) => model.findById(userId);
+    const findUserByUsername = (username) => model.findOne({ username: username });
+    const findUserByCredentials = (username, password) => model.findOne({ username, password });
+    const updateUser = (userId, user) => model.updateOne({ _id: userId }, { $set: user });
+    const deleteUser = (userId) => model.findByIdAndDelete(userId);
+    const findUsersByRole = (role) => model.find({ role }); // or just model.find({ role })
+    const findUsersByPartialName = (partialName) => {
+        const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
+        return model.find({
+            $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
+        });
     };
-    const findAllUsers = () => users;
-    const findUserById = (userId) => users.find((user) => user._id === userId);
-    const findUserByUsername = (username) => users.find((user) => user.username === username);
-    const findUserByCredentials = (username, password) =>
-        users.find((user) => user.username === username && user.password === password);
-    const updateUser = (userId, user) => (users = users.map((u) => (u._id === userId ? user : u)));
-    const deleteUser = (userId) => (users = users.filter((u) => u._id !== userId));
-    return {
-        createUser, findAllUsers, findUserById, findUserByUsername, findUserByCredentials, updateUser, deleteUser
-    };
+    return { createUser, findAllUsers, findUserById, findUserByUsername, findUserByCredentials, updateUser, deleteUser, findUsersByRole, findUsersByPartialName };
 }
-
--- index.js
+++ index.js
@@ -10,7 +10,10 @@ import AssignmentsRoutes from "./Kambaz/Assignments/routes.js";
 import EnrollmentsRoutes from "./Kambaz/Enrollments/routes.js";
 import "dotenv/config";
 import session from "express-session";
+import mongoose from "mongoose";
 
+const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
 
 const app = express()
 app.use(
