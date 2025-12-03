9eb1bd04e7aa45068bfd5629e9fd282398fbcdd2
Chris <125088905+wyattchris@users.noreply.github.com>
2025-12-02 22:38:54 -0500

    format

--- Kambaz/Assignments/schema.js
+++ Kambaz/Assignments/schema.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const assignmentSchema = new mongoose.Schema({
-        _id: String,
-        title: String,
-        course: { type: String, ref: "CourseModel" },
-        description: String,
-        points: Number,
-        availableFrom: Date,
-        availableUntil: Date,
-        due: Date,
+    _id: String,
+    title: String,
+    course: { type: String, ref: "CourseModel" },
+    description: String,
+    points: Number,
+    availableFrom: Date,
+    availableUntil: Date,
+    due: Date,
 },
     { collection: "assignments" }
 );
